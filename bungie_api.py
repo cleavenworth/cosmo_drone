e92f215b423ce7f63c771e9b1216e57d7cafb1fd
Cam Leavenworth <le@venworth.cam>
2019-03-06 14:00:33 -0500

    First commit

---
+++ bungie_api.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import urllib
+import os
+import json
+import requests
+
+
+# Bungie API Details
+API_KEY = os.environ['BUNGIE_KEY']
+HEADERS = {"X-API-Key": API_KEY}
+BASE_URL = "https://www.bungie.net/Platform/Destiny2/"
+
+class BungieLookup(object):
+    def __init__(self, players=None, discord_lookup=None, leaderboard=None):
+        if discord_lookup:
+            if len(players) == 0:
+                players = [""]
+            else:
+                players = players.split()
+            players[0] = self.read_bnet_user(discord_lookup)
+            self.players = players
+        elif leaderboard:
+            players = []
+            discord_users = self.TRIUMPH_SCORES['discord_users']
+            for discord_user in discord_users:
+                players.append(self.read_bnet_user(discord_user))
+            self.players = players
+        else:
+            players = players.split()
+            self.players = players
+
+    def get_bungie_membership_id(self, players):
+        member_ids = {}
+        for player in players:
+            try:
+                request = requests.get(BASE_URL + \
+                "SearchDestinyPlayer/4/" + \
+                urllib.parse.quote(player), \
+                headers=HEADERS)
+                member_ids[player] = request.json()['Response'][0]['membershipId']
+            except IndexError as exception:
+                print(request.url)
+                break
+        return member_ids
+
+    def get_triumph_score(self, member_ids):
+        scores = {}
+        for player in member_ids:
+            member_id = member_ids[player]
+            triumph_score = requests.get(BASE_URL  + \
+            "4/Profile/" + member_id + "/?components=Records", \
+            headers=HEADERS).json()['Response']['profileRecords']['data']['score']
+            scores[player] = {}
+            scores[player]['score'] = triumph_score
+            scores[player]['member_id'] = member_id
+        self.save_triumph_score(scores)
+        return scores
+
+    def save_triumph_score(self, scores):
+        for player in scores:
+            self.TRIUMPH_SCORES["triumph_scores"][player] = scores[player]
+        with open(self.JSON_PATH, 'w') as stored_scores:
+            json.dump(self.TRIUMPH_SCORES, stored_scores)
+
+    def load_stored_scores(json_path):
+        try:
+            with open(json_path, 'r')  as stored_scores:
+                triumph_scores = json.load(stored_scores)
+        except:
+            with open(json_path, 'w+') as stored_scores:
+                data = {"discord_users":{}, "triumph_scores":{}}
+                json.dump(data, stored_scores)
+                triumph_scores = json.load(stored_scores)
+        return triumph_scores
+
+    def read_bnet_user(self, discord_user):
+        try:
+            player = self.TRIUMPH_SCORES['discord_users'][discord_user]
+        except KeyError:
+            print("Unable to read registered BNet Username.")
+        return player
+
+    def register_bnet_user(self, player, discord_user):
+        try:
+            self.TRIUMPH_SCORES['discord_users'][discord_user] = player[0]
+        except KeyError:
+            self.TRIUMPH_SCORES["triumph_scores"][player[0]] = {}
+            self.TRIUMPH_SCORES['discord_users'][discord_user] = player[0]
+        self.get_triumph_score(self.get_bungie_membership_id([player[0]]))
+
+    def compare_triumph_score(self, players):
+        score_dict = self.get_triumph_score(self.get_bungie_membership_id(players))
+        score_data = [(player, data['score']) for player, data in score_dict.items()]
+        top_scorer_data = sorted(score_data, key=lambda x: x[1], reverse=True)[0]
+        score_data = sorted(score_data, key=lambda x: x[1], reverse=True)
+        return top_scorer_data,score_data
+
+    def triumph_leaderboard(self, players):
+        score_dict = self.get_triumph_score(self.get_bungie_membership_id(players))
+        score_data = [(player, data['score']) for player, data in score_dict.items()]
+        top_scorer_data = sorted(score_data, key=lambda x: x[1], reverse=True)[0]
+        score_data = sorted(score_data, key=lambda x: x[1], reverse=True)
+        return top_scorer_data, score_data
+
+    JSON_PATH = "stored_scores.json"
+    TRIUMPH_SCORES = load_stored_scores(JSON_PATH)
---
+++ cosmo_drone_bot.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+
+import os
+import asyncio
+import time
+import types
+import discord
+
+
+from bungie_api import BungieLookup as BL
+
+TOKEN = os.environ['DISCORD_KEY']
+# TOKEN = 'NTUxNTU4OTY5MDM1MDYzMzA5.D1ywFw.NA3Hmj4Ds83qXV26StPbveNY-ss'
+PERMISSION_INT = 84992
+
+cosmodrone = discord.Client()
+
+def format_lookup_message(score_dict):
+    msg = "\n"
+    for player in score_dict:
+        msg_part = "**{}** - Current Triumph Score: **{}**".format(player, score_dict[player]['score'])
+        msg = msg + "\n" + msg_part
+    return msg
+
+def format_compare_message(score_data):
+    msg = "\n"
+    top_scorer_data = [score_data[0][0], score_data[0][1]]
+    msg_part = "**{}** is in the lead with **{}** total Triumph Score.".format(top_scorer_data[0], top_scorer_data[1])
+    msg = msg + "\n" + msg_part
+    msg_part = "Highest to Lowest Scores:"
+    msg = msg + "\n" + msg_part
+    for player, score in score_data[1]:
+        msg_part = "**{}** - **{}**".format(player, score)
+        msg = msg + "\n" + msg_part
+    return msg
+
+def format_leaderboard_message(score_data):
+    msg = "\n"
+    top_scorer_data = [score_data[0][0], score_data[0][1]]
+    msg_part = "**{}** is in the lead of all registered users with **{}** total Triumph Score.".format(top_scorer_data[0], top_scorer_data[1])
+    msg = msg + "\n" + msg_part + "\n"
+    msg_part = "Highest to Lowest Scores:"
+    msg = msg + "\n" + msg_part
+    for player, score in score_data[1]:
+        msg_part = "**{}** - **{}**".format(player, score)
+        msg = msg + "\n" + msg_part
+    return msg
+
+def perform_triumph_action(action, player_list=None, discord_user=None):
+    try:
+        if len(player_list) == 0:
+            discord_flag = True
+            player_list = ""
+        if player_list.split()[0] == "me":
+            discord_flag = True
+        else:
+            discord_flag = False
+    except IndexError:
+        player_list = ""
+    if action == "lookup":
+        if discord_flag == True:
+            bl = BL(players=player_list, discord_lookup=discord_user)
+            triumph_scores = bl.get_triumph_score(bl.get_bungie_membership_id(bl.players))
+        else:
+            bl = BL(players=player_list)
+            triumph_scores = bl.get_triumph_score(bl.get_bungie_membership_id(bl.players))
+    elif action == "compare":
+        if discord_flag == True:
+            bl = BL(players=player_list, discord_lookup=discord_user)
+        else:
+            bl = BL(players=player_list)
+        triumph_scores = bl.compare_triumph_score(bl.players)
+    elif action == 'leaderboard':
+        bl = BL(leaderboard=True)
+        triumph_scores = bl.triumph_leaderboard(bl.players)
+    return triumph_scores
+
+def register_user(player, discord_user):
+    bl = BL(players=player)
+    bl.register_bnet_user(discord_user=discord_user, player=bl.players)
+
+# class Timer:
+#     def __init__(self, timeout, callback):
+#         self._timeout = timeout
+#         self._callback = callback
+#         self._task = asyncio.ensure_future(self._job())
+#
+#     async def _job(self):
+#         await asyncio.sleep(self._timeout)
+#         await self._callback()
+#
+#     def cancel(self):
+#         self._task.cancel()
+#
+#
+# async def timeout_callback():
+#     await asyncio.sleep(0.1)
+#     print('echo!')
+#
+#
+# async def main():
+#     print('\nfirst example:')
+#     timer = Timer(2, timeout_callback)  # set timer for two seconds
+#     await asyncio.sleep(2.5)  # wait to see timer works
+#
+#     print('\nsecond example:')
+#     timer = Timer(2, timeout_callback)  # set timer for two seconds
+#     await asyncio.sleep(1)
+#     timer.cancel()  # cancel it
+#     await asyncio.sleep(1.5)  # and wait to see it won't call callback
+
+
+# loop = asyncio.new_event_loop()
+# asyncio.set_event_loop(loop)
+# try:
+#     loop.run_until_complete(main())
+# finally:
+#     loop.run_until_complete(loop.shutdown_asyncgens())
+#     loop.close()
+
+
+
+@cosmodrone.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == cosmodrone.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await cosmodrone.send_message(message.channel, msg)
+
+    if message.content.startswith('!triumph_score'):
+        await cosmodrone.send_message(message.channel, "Scanning the Cosmo Drone...")
+        player_list = message.content[15:]
+        score_dict = perform_triumph_action(action="lookup", player_list=player_list, \
+        discord_user="{0.author}".format(message))
+        await cosmodrone.send_message(message.channel, "{0.author.mention}".format(message) + \
+        format_lookup_message(score_dict=score_dict))
+
+    if message.content.startswith('!triumph_compare'):
+        await cosmodrone.send_message(message.channel, "Scanning the Cosmo Drone...")
+        player_list = message.content[16:]
+        score_data = perform_triumph_action(action="compare", player_list=player_list, \
+        discord_user="{0.author}".format(message))
+        await cosmodrone.send_message(message.channel, "{0.author.mention}".format(message) + \
+        format_compare_message(score_data=score_data))
+
+    if message.content.startswith('!triumph_leaderboard'):
+        await cosmodrone.send_message(message.channel, "Scanning the Cosmo Drone Leaderboard... (This may take a few moments)")
+        player_list = message.content[20:]
+        score_data = perform_triumph_action(action="leaderboard", player_list=player_list, \
+        discord_user="{0.author}".format(message))
+        await cosmodrone.send_message(message.channel, "{0.author.mention}".format(message) + \
+        format_leaderboard_message(score_data=score_data))
+
+    if message.content.startswith('!triumph_register'):
+        player = message.content[18:]
+        await cosmodrone.send_message(message.channel, "Uploading Your BattleNet Tag to "\
+        "the Cosmo Drone...")
+        register_user(player, discord_user="{0.author}".format(message))
+        await cosmodrone.send_message(message.channel, "Your BattleNet Tag has been "\
+        "saved to your Discord user. You can now use `!triumph_score me` or just "\
+        "`!triumph_score` to view your current Triumph Score.")
+
+    if message.content.startswith('!triumph_vs_tracker'):
+        player_list = message.content[20:].split()
+        vs_list = " vs. ".join(f"**{x}**" for x in player_list)
+        print(vs_list)
+        print(player_list)
+        await cosmodrone.send_message(message.channel, "Setting Up Vs Tracker for {}".format(vs_list))
+
+# async def clock():
+#     while True:
+#         from datetime import timedelta, datetime
+#         cosmodrone.now.now = datetime.now()
+#         await asyncio.sleep(1)
+#         if
+
+
+@cosmodrone.event
+async def on_ready():
+    print('Logged in as', cosmodrone.user.name)
+    print(cosmodrone.user.id)
+    print('------')
+
+# cosmodrone.now = types.MethodType( cosmodrone, clock )
+# cosmodrone.loop.create_task(clock())
+cosmodrone.run(TOKEN)
